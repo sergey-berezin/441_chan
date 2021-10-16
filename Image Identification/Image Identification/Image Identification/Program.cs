c66731622b4ee14d4dd122a3386340b3ed8c21e0
chuyunduan <chuyunduan@gmail.com>
2021-10-16 17:31:48 +0800

    FIX
    FIX

---
+++ Image Identification/Image Identification/Image Identification/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using YOLOv4;
+
+namespace Image_Identification
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Please enter the folder path : ");
+            string imageFolder = Console.ReadLine();
+
+            using (var picture = new Processing())
+            {
+                await foreach (var processResult in picture.ProcessImagesAsync(imageFolder))
+                    Console.WriteLine(processResult);
+            }
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-Это первое задание 1а, Чень Жуйпэн 441/2 в седьмом семестре.
-![Image text](https://github.com/chuyunduan/441_chan/blob/master/%D1%80%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82.png)
